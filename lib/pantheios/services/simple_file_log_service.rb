3ab2daae8f71cb409c85d95745c1adacd0a4e8bc
Matt <matthew@synesis.com.au>
2018-02-04 18:05:28 +1100

    + added SimpleFileLogService

---
+++ lib/pantheios/services/simple_file_log_service.rb
@@ -0,0 +1,192 @@
+
+# ######################################################################## #
+# File:         lib/pantheios/services/simple_file_log_service.rb
+#
+# Purpose:      Definition of the
+#               ::Pantheios::Services::SimpleFileLogService class
+#
+# Created:      17th June 2015
+# Updated:      4th February 2018
+#
+# Home:         http://github.com/synesissoftware/Pantheios-Ruby
+#
+# Author:       Matthew Wilson
+#
+# Copyright (c) 2015-2018, Matthew Wilson and Synesis Software
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the names of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# ######################################################################## #
+
+
+require 'pantheios/application_layer/stock_severity_levels'
+
+require 'logger'
+
+=begin
+=end
+
+module Pantheios
+module Services
+
+# A class that fulfils the Pantheios *LogService* protocol that allows all
+# severities and logs to a file
+#
+# NOTE: The *LogService* protocol is implemented by a class that provides
+# the instance methods +severity_logged?(severity : Object) : boolean+ and
+# +log(severity : Object, t : Time, prefix : String, msg : String)+
+class SimpleFileLogService
+
+	module SimpleFileLogService_Constants
+
+		DEFAULT_ROLL_DEPTH		=	7
+		DEFAULT_ROLL_SIZE		=	1024 * 1024
+
+	end # module SimpleFileLogService_Constants
+
+	#
+	# === Signature
+	#
+	# * *Parameters:*
+	#   - +log_file_or_path+:: [ +::File+, +::IO+, +::String+ ] A file or an
+	#     IO that will be used as the target of the log statements, or a
+	#     string specifying the path of the file to be used as the target
+	#   - +options+:: [ ::Hash ] Options
+	#
+	# * *Options:*
+	#   - +:roll_period+:: ( +:daily+, +:weekly+, +:monthly+ ) The
+	#     periodicity of the log-file rolling. Ignored unless
+	#     +log_file_or_path+ is a +::String+. Ignored if either +:roll_size+
+	#     or +:roll_depth+ is specified
+	#   - +:roll_size+:: [ ::Integer, [ ::Integer, ::Integer ] ] An integer
+	#     specifying the size of the log file, or an array containing the
+	#     size of the log file and the depth of the log roll
+	#   - +:roll_depth+:: [ ::Integer ] The depth of the size-based log
+	#     rolling. Overrides the second element in an array specified for
+	#     +:roll_size+ 
+
+
+	def initialize log_file_or_path, **options
+
+		roll_period	=	options[:roll_period]
+		roll_size	=	options[:roll_size]
+		roll_depth	=	options[:roll_depth]
+
+		if roll_period && (roll_size || roll_depth)
+
+			warn "caller specified :roll_depth/:roll_period with :roll_size to #{self.class}##{__method__}() - ignoring :roll_period"
+
+			roll_period = nil
+		end
+
+		if roll_size || roll_depth
+
+			if ::Array === roll_size
+
+				roll_size, d	=	roll_size
+
+				roll_depth		||=	d
+			end
+
+			roll_size	||=	SimpleFileLogService_Constants::DEFAULT_ROLL_SIZE
+			roll_depth	||=	SimpleFileLogService_Constants::DEFAULT_ROLL_DEPTH
+		end
+
+		logger_init_args	=	[]
+		logger_init_options	=	{}
+
+		if false;
+		elsif roll_depth
+
+			logger_init_args	<<	roll_depth
+			logger_init_args	<<	roll_size
+		elsif roll_period
+
+			roll_period = roll_period.downcase.to_sym if ::String === roll_period
+
+			case roll_period
+			when :daily, :weekly, :monthly
+
+				;
+			else
+
+				raise ArgumentError, ":roll_period value must be one of :daily, :weekly, :monthly"
+			end
+
+			logger_init_args	<<	roll_period.to_s
+			logger_init_args	<<	0
+		end
+
+
+		case log_file_or_path
+		when nil
+
+			raise ArgumentError, 'log_file_or_path may not be nil'
+		when ::File, ::Tempfile
+
+			@logger			=	::Logger.new log_file_or_path, *logger_init_args
+			@log_file_path	=	log_file_or_path.path
+		when ::IO, ::StringIO
+
+			@logger			=	::Logger.new log_file_or_path, *logger_init_args
+			@log_file_path	=	log_file_or_path.respond_to?(:path) ? log_file_or_path.path : nil
+		when ::String
+
+			@logger			=	::Logger.new log_file_or_path, *logger_init_args
+			@log_file_path	=	log_file_or_path
+		else
+
+			raise TypeError, "log_file_or_path type must be one of #{::File}, #{::IO}, #{::String}, #{::StringIO}"
+		end
+
+		self
+	end
+
+	# The path of the underlying log file. May return +nil+ if the path
+	# cannot be determined
+	attr_reader	:log_file_path
+
+	def severity_logged? severity
+
+		true
+	end
+
+	def log sev, t, pref, msg
+
+		@logger << "#{pref}#{msg}\n"
+	end
+end
+
+
+end # module Services
+end # module Pantheios
+
+# ############################## end of file ############################# #
+
+
---
+++ test/unit/services/tc_simple_file_log_service.rb
@@ -0,0 +1,238 @@
+#!/usr/bin/env ruby
+#
+# test/unit/services/tc_simple_file_log_service.rb
+
+$:.unshift File.join(File.dirname(__FILE__), '../../..', 'lib')
+
+require 'pantheios/services/simple_file_log_service'
+
+require 'pantheios/application_layer/stock_severity_levels'
+
+require 'xqsr3/extensions/test/unit'
+
+require 'test/unit'
+
+require 'stringio'
+require 'tempfile'
+
+class Test_SimpleFileLogservice < Test::Unit::TestCase
+
+	include ::Pantheios::Services
+
+	def test_SimpleFileLogService_type_exists
+
+		assert defined? SimpleFileLogService
+	end
+
+	if  defined?(SimpleFileLogService)
+
+		def self.log_to_StringIO_and_get_output messages, def_sev, def_pref = nil, def_t = nil, **options
+
+			def_t		||=	Time.now
+			def_pref	||=	"[prog, thread, #{def_t}]: "
+
+			stream		=	StringIO.new
+
+			svc			=	SimpleFileLogService.new stream, **options
+
+			messages.each do |message|
+
+				ar		=	::Array === message ? message : [ message ]
+
+				msg		=	ar[0]
+				sev		=	ar[1] || def_sev
+				pref	=	ar[2] || def_pref
+				t		=	ar[3] || def_t
+
+				svc.log sev, t, pref, msg
+			end
+
+			stream.string
+		end
+
+		def self.log_to_Tempfile_and_get_output messages, def_sev, def_pref = nil, def_t = nil, **options
+
+			def_t		||=	Time.now
+			def_pref	||=	"[prog, thread, #{def_t}]: "
+
+			tf			=	Tempfile.new 'Pantheios.Ruby.Tempfile'
+
+			begin
+
+				svc			=	SimpleFileLogService.new tf, **options
+
+				messages.each do |message|
+
+					ar		=	::Array === message ? message : [ message ]
+
+					msg		=	ar[0]
+					sev		=	ar[1] || def_sev
+					pref	=	ar[2] || def_pref
+					t		=	ar[3] || def_t
+
+					svc.log sev, t, pref, msg
+				end
+
+				tf.rewind
+
+				return tf.read
+			ensure
+
+				tf.close
+				tf.unlink
+			end
+		end
+
+		def self.log_to_Tempfile_path_and_get_output messages, def_sev, def_pref = nil, def_t = nil, **options
+
+			def_t		||=	Time.now
+			def_pref	||=	"[prog, thread, #{def_t}]: "
+
+			tf			=	Tempfile.new 'Pantheios.Ruby.Tempfile'
+
+			begin
+
+				svc			=	SimpleFileLogService.new tf.path, **options
+
+				messages.each do |message|
+
+					ar		=	::Array === message ? message : [ message ]
+
+					msg		=	ar[0]
+					sev		=	ar[1] || def_sev
+					pref	=	ar[2] || def_pref
+					t		=	ar[3] || def_t
+
+					svc.log sev, t, pref, msg
+				end
+
+				tf.rewind
+
+				return tf.read
+			ensure
+
+				tf.close
+				tf.unlink
+			end
+		end
+
+		def test_SimpleFileLogService_type_is_a_class
+
+			assert_kind_of(::Class, SimpleFileLogService)
+		end
+
+		def test_SimpleFileLogService_type_has_expected_instance_methods
+
+			assert_type_has_instance_methods SimpleFileLogService, [ :severity_logged?, :log ]
+		end
+
+		def test_severity_logged_false_with_large_range_of_integers
+
+			output	=	StringIO.new
+
+			svc = SimpleFileLogService.new output
+
+			(-10000 .. 10000).each do |sev|
+
+				assert_true(svc.severity_logged?(sev), "severity '#{sev}' (#{sev.class}) was not logged")
+			end
+
+#			lines	=	output.string.split /\n/
+		end
+
+		def test_simple_logging_with_StringIO_1
+
+			message	=	'msg'
+
+			output	=	self.class.log_to_StringIO_and_get_output [ message ], :notice, nil, nil
+
+			lines	=	output.split /\n/
+
+			assert_not_empty lines
+			assert_equal 1, lines.size
+			assert_match /msg$/, lines[0]
+		end
+
+		def test_simple_logging_with_StringIO_2
+
+			msgs	=	[
+
+				[ 'msg-1' ],
+				[ 'msg-2' ],
+			]
+
+			output	=	self.class.log_to_StringIO_and_get_output msgs, :notice, nil, nil
+
+			lines	=	output.split /\n/
+
+			assert_not_empty lines
+			assert_equal 2, lines.size
+			assert_match /msg-1$/, lines[0]
+			assert_match /msg-2$/, lines[1]
+		end
+
+		def test_simple_logging_with_Tempfile_1
+
+			message	=	'msg'
+
+			output	=	self.class.log_to_Tempfile_and_get_output [ message ], :notice, nil, nil
+
+			lines	=	output.split /\n/
+
+			assert_not_empty lines
+			assert_equal 1, lines.size
+			assert_match /msg$/, lines[0]
+		end
+
+		def test_simple_logging_with_Tempfile_2
+
+			msgs	=	[
+
+				[ 'msg-1' ],
+				[ 'msg-2' ],
+			]
+
+			output	=	self.class.log_to_Tempfile_and_get_output msgs, :notice, nil, nil
+
+			lines	=	output.split /\n/
+
+			assert_not_empty lines
+			assert_equal 2, lines.size
+			assert_match /msg-1$/, lines[0]
+			assert_match /msg-2$/, lines[1]
+		end
+
+		def test_simple_logging_with_Tempfile_path_1
+
+			message	=	'msg'
+
+			output	=	self.class.log_to_Tempfile_path_and_get_output [ message ], :notice, nil, nil
+
+			lines	=	output.split /\n/
+
+			assert_not_empty lines
+			assert_equal 1, lines.size
+			assert_match /msg$/, lines[0]
+		end
+
+		def test_simple_logging_with_Tempfile_path_2
+
+			msgs	=	[
+
+				[ 'msg-1' ],
+				[ 'msg-2' ],
+			]
+
+			output	=	self.class.log_to_Tempfile_path_and_get_output msgs, :notice, nil, nil
+
+			lines	=	output.split /\n/
+
+			assert_not_empty lines
+			assert_equal 2, lines.size
+			assert_match /msg-1$/, lines[0]
+			assert_match /msg-2$/, lines[1]
+		end
+	end
+end
+
+
